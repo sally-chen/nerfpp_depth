43285caf3637ff5aef75673a28542b189e9da365
sally-chen <chuhan.chen@mail.utoronto.ca>
2021-08-30 11:46:33 -0400

    net depth=4 for box

--- ddp_model.py
+++ ddp_model.py
@@ -669,7 +669,7 @@ class NerfNetMoreBox(nn.Module):
         # this is the net for the box
 
         # input to this should be box position and sampled position
-        self.box_net = MLPNet(D=args.netdepth, W=args.netwidth,
+        self.box_net = MLPNet(D=4, W=args.netwidth,
                               input_ch=self.fg_embedder_position.out_dim,
                               input_ch_viewdirs=self.fg_embedder_viewdir.out_dim,
                               use_viewdirs=args.use_viewdirs)
